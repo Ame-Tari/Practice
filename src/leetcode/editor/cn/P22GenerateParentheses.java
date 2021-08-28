5e027774be44bca69128f913d028aff04c5974db
Ame-Tari <878027460@qq.com>
2021-08-28 16:53:33 +0800

    1.括号生成(解答)

--- src/leetcode/editor/cn/P22GenerateParentheses.java
+++ src/leetcode/editor/cn/P22GenerateParentheses.java
@@ -35,7 +35,7 @@ package leetcode.editor.cn;
 import java.util.ArrayList;
 import java.util.List;
 
-public class P22GenerateParentheses{
+public class P22GenerateParentheses {
     public static void main(String[] args) {
         Solution solution = new P22GenerateParentheses().new Solution();
         // TO TEST
@@ -44,27 +44,29 @@ public class P22GenerateParentheses{
     //leetcode submit region begin(Prohibit modification and deletion)
 
     class Solution {
-        ArrayList<String> res = new ArrayList<>();
+        List<String> res = new ArrayList<>();
         public List<String> generateParenthesis(int n) {
-            if (n <= 0) {
+            if(n <= 0){
                 return res;
             }
-            getParenthesis("", n, n);
+            getParenthesis("",n,n);
             return res;
         }
 
-        private void getParenthesis(String str, int left, int right) {
-            if (left == 0 && right == 0) {
+        private void getParenthesis(String str,int left, int right) {
+            if(left == 0 && right == 0 ){
                 res.add(str);
                 return;
             }
-            if (left == right) {
-                getParenthesis(str+"(", left--, right);
-            } else if (left < right) {
-                if (left > 0) {
-                    getParenthesis(str+"(",left--,right);
+            if(left == right){
+                //剩余左右括号数相等，下一个只能用左括号
+                getParenthesis(str+"(",left-1,right);
+            }else if(left < right){
+                //剩余左括号小于右括号，下一个可以用左括号也可以用右括号
+                if(left > 0){
+                    getParenthesis(str+"(",left-1,right);
                 }
-                getParenthesis(str + ")", left, right--);
+                getParenthesis(str+")",left,right-1);
             }
         }
     }
